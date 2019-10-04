7551a60bc79ac1844b25b089387ae2c4a970e26a
olivier <olivier.terrier@kairntech.com>
2019-10-04 11:36:05 +0200

    SHERPA-234 #resolve

--- app/pipeline/RuleSentencizer.py
+++ app/pipeline/RuleSentencizer.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+
+import plac
+import spacy
 from spacy.matcher.matcher import Matcher
 from spacy.tokens.doc import Doc
 
@@ -60,6 +64,40 @@ class RuleSentencizer(object):
                 for token in doc[start:end]:
                     if token.is_sent_start:
                         token.is_sent_start = False
+        if doc.is_sentenced:
+            # Trim starting spaces
+            sent_start = None
+            for sent in doc.sents:
+                sentlen = len(sent)
+                first_non_space = 0
+                while first_non_space < sentlen and sent[first_non_space].is_space:
+                    first_non_space += 1
+                if first_non_space > 0:
+                    sent[0].is_sent_start = False
+                    sent[first_non_space].is_sent_start = True
+
         doc.is_parsed = save_parsed if doc.is_sentenced else True
         return doc
 
+
+def main():
+    text = "toto\net titi"
+    #text = "L'annonce du divorce de Sarkozy \"aurait pu se faire plus tôt\": Hollande \n \nMULHOUSE, 18 oct 2007 (AFP) \n \nL'annonce du divorce de Sarkozy \"aurait pu se faire plus tôt\": Hollande \n \nL'annonce du divorce de Nicolas et Cécilia Sarkozy \"aurait pu se faire plus tôt\", a estimé jeudi le premier secrétaire du Parti socialiste pour qui \"il fallait qu'il y ait une clarification\". \n \n\"Désormais, il n'y aura plus de questions qui seront posées\", a souligné M. Hollande, lors d'une conférence de presse organisée en marge d'une visite à la fédération du PS du Haut-Rhin. \n \n\"Cette annonce aurait pu se faire plus tôt. Mais je veux croire à la coïncidence\" avec le mouvement de grève de jeudi dans le pays, a-t-il dit. \n \n\"Aujourd'hui, l'information principale n'est pas le divorce de M. et Mme Sarkozy, c'est la grève particulièrement suivie qui a donné l'espoir qu'après ce mouvement il puisse y avoir une vraie négociation\".\nM. Hollande a aussi estimé que \"M. Sarkozy est un homme comme les autres\". \"Il a fait un choix personnel et sa vie privée mérite protection\", a-t-il dit."
+    overrides = defaultdict(dict)
+    overrides["rule_sentencizer"]["split"] = [
+        # Split on double line breaks
+        [{"IS_SPACE": True, "TEXT": { "REGEX" : "([\r]?[\n]){2,}" }}, {}],
+        # Split on hard punctuation
+        [{"ISPUNCT": True, "TEXT" : { "IN" : [".", "!", "?"]}}, {}],
+        # Split on full stop if not followed by lower case letter or digit
+        [{"ISPUNCT": True, "TEXT": "."}, {"SHAPE": { "REGEX" : "^[^xd]" }}]
+    ]
+    nlp = spacy.load("fr", disable=['ner', 'parser'])
+    custom = RuleSentencizer(nlp, **overrides)
+    nlp.add_pipe(custom)
+    doc = nlp(text)
+    for sent in doc.sents:
+        print("<%s>"%sent.text)
+
+if __name__ == "__main__":
+    plac.call(main)
